7f63226f8856a784d279cb24a9de57f07c4ed953
beafraidnotlearning <beafraidnotlearning@mail.ru>
2020-10-02 15:53:40 +0300

    feat: add implementation of the min method

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+exports.min = function min(array) {
+    if (typeof array != "undefined") {
+        if (array.length != 0) {
+            return array.reduce(function (pv, cv) {
+                return cv < pv ? cv : pv;
+            });
+        } else {
+            return 0;
+        }
+    } else {
+        return 0;
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {};
